d337fb78f1ae503d404f075841acd7c349a51108
mozzam123 <mozzam607@gmail.com>
2023-10-25 16:16:29 +0530

    Data Class

---
+++ DataClass.py
@@ -0,0 +1,22 @@
+"""
+The main difference between data class and python regular class is that a data class has the methods __init__,
+__eq__ and __repr__ already implemented.
+
+"""
+
+
+from dataclasses import dataclass
+
+@dataclass
+class Vehicle:
+    name: str
+    seats: int
+    
+    @property
+    def about(self):
+        print( f"My vehicle name is {self.name} and have {self.seats} seats")
+
+
+
+car = Vehicle("toyota", 5)
+car.about
