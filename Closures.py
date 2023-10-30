083f19b38089722567ce391875a48521be605d5f
mozzam123 <mozzam607@gmail.com>
2023-10-30 20:45:24 +0530

    Closures

---
+++ Closures.py
@@ -0,0 +1,33 @@
+"""
+For example, if you have a function that makes another function and gives it a number, that inner
+function can still remember and use that number even after the outer function has finished its job.
+This special ability is what we call a "closure." It helps you keep and use important information in your code.
+"""
+
+# First Example
+def show_name(firstname, lastname):
+    def concatenate(names):
+        return " ".join(names)
+    return concatenate(names=[firstname, lastname])
+
+print(show_name(firstname="mozzam", lastname="inamdar"))
+
+
+## Second Example
+def street_name():
+    def concatenate(names):
+        return " ".join(names)
+    return concatenate
+
+street = street_name()
+print(street(names=["high", "lane"]))
+
+
+## Third Example
+def name_maker(names):
+    def concatenate():
+        return " ".join(names)
+    return concatenate
+
+name = name_maker(names=['moz', 'tea'])
+print(name())
