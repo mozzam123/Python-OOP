3b398c0c91cd1001ed9507f0baf5d842ed8d11ee
mozzam123 <mozzam607@gmail.com>
2024-02-05 12:02:30 +0530

    Encapsulation in Python

---
+++ Encapsulation.py
@@ -0,0 +1,30 @@
+"""
+Encapsulation is one of the fundamental principles of object-oriented programming (OOP) that involves
+bundling the data (attributes) and the methods (functions) that operate on the data into a single unit called a class.
+It allows the data to be hidden and accessed only through the defined methods, which provides a way to control access to the data and protects it from unintended modification.
+
+"""
+
+
+
+
+class Car:
+    def __init__(self, make, model):
+        self.__make = make  # private attribute
+        self.__model = model  # private attribute
+        self.__speed = 0  # private attribute
+
+    def accelerate(self, increment):
+        self.__speed += increment
+
+    def get_speed(self):
+        return self.__speed
+
+# Creating an instance of the Car class
+my_car = Car(make="Toyota", model="Camry")
+
+# Accessing attributes through methods
+my_car.accelerate(20)
+current_speed = my_car.get_speed()
+
+print(f"My {my_car._  } {my_car._Car__model}'s current speed is {current_speed} mph.")
