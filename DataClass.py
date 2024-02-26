6dba83206f6f0d35aac8c3c92dfbcb3378819293
mozzam123 <mozzam607@gmail.com>
2024-02-26 13:23:00 +0530

    Made changes in dataclass

--- DataClass.py
+++ DataClass.py
@@ -1,4 +1,5 @@
 """
+In Python, the dataclass module provides a simple way to create classes primarily used for storing data.
 The main difference between data class and python regular class is that a data class has the methods __init__,
 __eq__ and __repr__ already implemented.
 
@@ -12,6 +13,10 @@ class Vehicle:
     name: str
     seats: int
     
+    @property
+    def getName(self):
+        print(self.name)
+
     @property
     def about(self):
         print( f"My vehicle name is {self.name} and have {self.seats} seats")
