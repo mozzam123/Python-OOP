e509a08915102d8976cc743758b188371bf518d6
mozzam123 <mozzam607@gmail.com>
2024-02-26 11:44:18 +0530

    Types of inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,4 +1,6 @@
-# Parent class
+# Type: Single Inheritance
+" Single inheritance occurs when a class inherits from only one base class. "
+
 class Animal():
 
     def __init__(self, name):
@@ -10,5 +12,60 @@ class Cat(Animal):
     def speak(self):
         print(f"Hello from {self.name} which has {self.legs} legs")
 
-cat = Cat("leo")
-cat.speak()
+
+# Type: Multiple Inheritance
+" Multiple inheritance occurs when a class inherits from more than one base class. "
+
+class Mammal():
+    def isType(self):
+        print("type of mammal")
+
+class Animal():
+
+    def __init__(self, name):
+        self.name = name
+        self.legs = 4
+
+
+class Cat(Animal, Mammal):
+    def speak(self):
+        print(f"Hello from {self.name} which has {self.legs} legs")
+
+
+# Type: Multi-level Inheritance
+" Multilevel inheritance occurs when a class is derived from a base class, and then another class is derived from that derived class. "
+        
+class Mammal():
+        def isType(self):
+            print("type of mammal")
+
+class Animal(Mammal):
+
+    def __init__(self, name):
+        self.name = name
+        self.legs = 4  
+
+class Cat(Animal):
+    def speak(self):
+        print(f"Hello from {self.name} which has {self.legs} legs")
+
+
+
+# Hierarchical Inheritance
+        
+" Hierarchical inheritance occurs when more than one class is derived from a single base class. "
+        
+
+class Mammal():
+        def isType(self):
+            print("type of mammal")
+
+class Animal(Mammal):
+
+    def __init__(self, name):
+        self.name = name
+        self.legs = 4  
+
+class Cat(Mammal):
+    def speak(self):
+        print(f"Hello from {self.name} which has {self.legs} legs")
